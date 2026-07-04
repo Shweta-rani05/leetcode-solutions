c95e15af25612cfb2b9aa0b9706abcd4ea398168
Shweta Rani <ranishweta96936@gmail.com>
2026-07-04 19:52:34 +0530

    Time: 13 ms (10.76%), Space: 20.7 MB (23.98%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) {
-        int ans = 0;
+    int singleNumber(vector<int>& num1) {
+        int ans = 0 ;
 
-        for(int val : nums ){
+        for(int val : num1){
             ans = ans^val ;
         }
         return ans;
-        
     }
 };
